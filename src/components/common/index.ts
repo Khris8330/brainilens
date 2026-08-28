b4bf6eb7cb3ec82d8f65994a1e8aa356f7e3f294
Christian Chiemerie <111049690+Khris8330@users.noreply.github.com>
2026-08-28 11:41:19 +0100

    chore: export BrandLogo from common components

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -8,3 +8,6 @@ export { Header } from './Header'
 export type { HeaderProps } from './Header'
 
 export { Footer } from './Footer'
+
+export { BrandLogo, BrandMark } from './BrandLogo'
+export type { BrandLogoProps } from './BrandLogo'
